27c4e90938fb6ef5e166c8f7dea65177973c8e41
david-3lm <91316949+david-3lm@users.noreply.github.com>
2021-11-20 17:18:38 +0100

    Inicio fase 3

---
+++ SR-Servidor/src/main/java/com/example/demo/JuegilloApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegilloApplication.class, args);
+	}
+
+}
