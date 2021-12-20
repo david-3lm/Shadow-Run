03c5a23ed8414e5035a1ff72b01bb34957bcfd8e
NeoTX14 <74070655+NeoTX14@users.noreply.github.com>
2021-12-20 15:05:58 +0100

    Add files via upload
    WS java

---
+++ SR-Servidor/src/main/java/com/example/demo/WsJueguillo.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@SpringBootApplication
+@EnableWebSocket
+public class WsJueguillo implements WebSocketConfigurer{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WsJueguillo.class, args);
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(createGameHandler(), "/game")
+			.setAllowedOrigins("*");
+	}
+	
+	@Bean
+	public GameHandler createGameHandler() {
+		return new GameHandler();
+	}
+
+}
